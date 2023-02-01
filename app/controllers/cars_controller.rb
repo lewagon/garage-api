f12f95b5dbd882ac27baf3cd43ffc9137e717574
Anne de Joly <anne.dejoly@edhec.com>
2023-02-01 16:44:23 +0000

    Extra space

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,6 @@
 class CarsController < ApplicationController
   def index
-    @cars = Car.where(garage:  params[:garage])
+    @cars = Car.where(garage: params[:garage])
     respond_to do |format|
       format.html { render json: @cars }
       format.xml { render xml: @cars.as_json }
